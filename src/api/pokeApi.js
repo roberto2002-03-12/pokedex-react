7031a2ce1f43241930eca5abf96ae3be262e7774
roberto2002-03-12 <emilio.contreras@tecsup.edu.pe>
2023-05-04 15:50:48 -0500

    quitando env

--- src/api/pokeApi.js
+++ src/api/pokeApi.js
@@ -1,7 +1,4 @@
 import axios from 'axios';
-import { getEnvVariable } from '../helpers/getEnvVariable';
-
-const { POKE_API_URL } = getEnvVariable();
 
 const pokeApi = axios.create({
     baseURL: 'https://pokeapi.co/api/v2'
--- src/helpers/getEnvVariable.js
+++
@@ -1,7 +0,0 @@
-export const getEnvVariable = () => {
-    import.meta.env;
-
-    return {
-        ...import.meta.env
-    };
-};
